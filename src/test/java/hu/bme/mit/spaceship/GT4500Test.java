32ff985120692e31b3eaf029852cff6dec7a705e
CocaCola97 <kristi.hofe@gmail.com>
2018-05-11 04:51:53 -0700

    Added more tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -153,6 +153,31 @@ public class GT4500Test {
   }
 
   @Test
+  public void fireTorpedo_Single_Success_PrimaryEmpty(){
+    // Arrange
+    when(primary.isEmpty()).thenReturn(true);
+    when(primary.fire(1)).thenReturn(false);
+    when(secondary.isEmpty()).thenReturn(false);
+    when(secondary.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    verify(primary, times(0)).fire(1);
+    verify(secondary, times(1)).fire(1);
+
+    assertEquals(true, result);
+
+    result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    verify(primary, times(0)).fire(1);
+    verify(secondary, times(2)).fire(1);
+
+    assertEquals(true, result);
+
+  }
+
+  @Test
   public void fireTorpedo_All_Failure_SecondaryFails(){
     // Arrange
     when(primary.isEmpty()).thenReturn(false);
@@ -165,6 +190,41 @@ public class GT4500Test {
 
     verify(primary, times(1)).fire(1);
     verify(secondary, times(1)).fire(1);
+
+    assertEquals(false, result);
+
+  }
+
+  @Test
+  public void fireLaser_Test() {
+    boolean result = ship.fireLaser(FiringMode.ALL);
+
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireTorpedo_Single_PrimaryEmptySecondTime(){
+    // Arrange
+    when(primary.isEmpty()).thenReturn(false);
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.isEmpty()).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(false);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(0)).fire(1);
+
+    assertEquals(true, result);
+
+    when(primary.isEmpty()).thenReturn(true);
+    when(primary.fire(1)).thenReturn(false);
+
+    result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(0)).fire(1);
 
     assertEquals(false, result);
 
